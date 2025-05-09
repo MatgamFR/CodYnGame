06679232d03baaa20e77512f19f394451f96cd44
MatgamFR <80062640+MatgamFR@users.noreply.github.com>
2025-05-09 11:11:46 +0200

    Delete src/main/java/java directory

--- src/main/java/java/IDEExecuteCode.java
+++
@@ -1,8 +0,0 @@
-public abstract class IDEExecuteCode {
-    /**
-     * Exécute le code Python donné en paramètre.
-     * @param code Le code à exécuter
-     */
-    public abstract void executeCode(String code);
-    public abstract void compileCode(String code);
-}
--- src/main/java/java/UserProgram.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-
-public class UserProgram {
-    public static void main(String[] args) {
-        System.out.println("helloworld");
-    }
-}
