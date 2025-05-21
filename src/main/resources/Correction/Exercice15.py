7ecd0103fbdba8d466dc005f5909b811215ce496
Leonchen43 <106692418+Leonchen43@users.noreply.github.com>
2025-05-21 18:01:21 +0200

    *et/

---
+++ src/main/resources/Correction/Exercice15.py
@@ -0,0 +1,11 @@
+word = input().replace('\\n', '\n').split('\n')
+
+a=input()
+b=input()
+if int(word[0]) == int(a)*int(b):
+    print(1)
+else:
+    print(0)
+    print(word[0])
+    print(int(a)*int(b))
+    print(1)
