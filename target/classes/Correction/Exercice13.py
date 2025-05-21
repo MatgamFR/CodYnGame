9a930ff70191b7eddff880314f054275a696b14f
Leonchen43 <106692418+Leonchen43@users.noreply.github.com>
2025-05-21 17:15:35 +0200

    mat

--- target/classes/Correction/Exercice13.py
+++ target/classes/Correction/Exercice13.py
@@ -1,1 +1,11 @@
 word = input().replace('\\n', '\n').split('\n')
+
+a=input()
+b=input()
+if word[0] = int(a)+int(b):
+    print(1)
+else:
+    print(0)
+    print(word[0])
+    print(int(a)+int(b))
+    print(1)
